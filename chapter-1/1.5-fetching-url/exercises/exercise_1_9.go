d0b468176e754188906d3311be43b8df624be76b
David McQueen <david@dmcqueen.co.uk>
2019-03-14 19:17:03 +0000

    Chapter 1 Section 1.5 & Exercises
    Fetch a URL and print it out to the terminal

---
+++ chapter-1/1.5-fetching-url/exercises/exercise_1_9.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+	"strings"
+)
+
+// Also print the response Status Code
+
+func main() {
+	for _, url := range os.Args[1:] {
+
+		urlToUse := url
+
+		if !strings.HasPrefix(urlToUse, "http://") {
+			fmt.Fprintf(os.Stderr, "fetch: Adding Prefix\n")
+			urlToUse = "http://" + urlToUse
+		}
+
+		resp, err := http.Get(urlToUse)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "fetch: %v\n", err)
+			os.Exit(1)
+		}
+
+		//b, err := ioutil.ReadAll(resp.Body)
+
+		c, err := io.Copy(os.Stdout, resp.Body)
+
+
+		resp.Body.Close()
+		fmt.Printf("\f", c)
+
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "fetch: copying %s: %v\n", url, err)
+			os.Exit(1)
+		}
+		fmt.Printf("fetch: Status Code: %d", resp.StatusCode)
+	}
+}
